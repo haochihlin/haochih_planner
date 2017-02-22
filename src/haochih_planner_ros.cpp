4b245866ce8a4631f1797c2635a1901da6f62597
HaoChih LIN <f44006076@gmail.com>
2017-02-22 11:37:02 +0800

    Fix bugs

--- src/haochih_planner_ros.cpp
+++ src/haochih_planner_ros.cpp
@@ -189,6 +189,9 @@ namespace haochih_local_planner
         break;
     }
     
+    if(accumulate_dist <= l1_dist)
+      return false; //it represents the robot is approaching the goal
+
     if(updated_l1_plan.size() >= 1)
       return this->dwaObj_->setPlan(updated_l1_plan); //Send l1-based plan to DWA planner
     else
